78f945224ced620ef92f2ad5783079f5c82821d8
LiYuqing <im.yuqlee@gmail.com>
2023-03-21 22:22:14 +0800

    Decode memo

--- node.go
+++ node.go
@@ -1,13 +1,40 @@
 package safe
 
 import (
+	"encoding/hex"
 	"fmt"
+	"unicode/utf8"
 
 	"github.com/MixinNetwork/trusted-group/mtg"
+	"github.com/gofrs/uuid"
 )
 
+type MixinExtraPack struct {
+	T uuid.UUID
+	M string `msgpack:",omitempty"`
+}
+
+func DecodeMixinExtra(b []byte) *MixinExtraPack {
+	var p MixinExtraPack
+	err := mtg.MsgpackUnmarshal(b, &p)
+	if err == nil && (p.M != "" || p.T.String() != uuid.Nil.String()) {
+		return &p
+	}
+	if utf8.Valid(b) {
+		p.M = string(b)
+	} else {
+		p.M = hex.EncodeToString(b)
+	}
+	return &p
+}
+
 func DecodeExtra(aesKey []byte, memo string) (*Operation, error) {
-	msp := mtg.DecodeMixinExtra(memo)
+	memoBuf, err := hex.DecodeString(memo)
+	if err != nil {
+		return nil, err
+	}
+	mep := DecodeMixinExtra(memoBuf)
+	msp := mtg.DecodeMixinExtra(mep.M)
 	if msp == nil {
 		return nil, fmt.Errorf("empty memo")
 	}
