28f02b0f38483f445089825a394849606944504c
LiYuqing <im.yuqlee@gmail.com>
2023-03-27 15:49:55 +0700

    Read deposits

---
+++ client/deposit.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Deposit struct {
+	Amount          string    `json:"amount"`
+	AssetID         string    `json:"asset_id"`
+	Chain           int64     `json:"chain"`
+	OutputIndex     int64     `json:"output_index"`
+	Receiver        string    `json:"receiver"`
+	TransactionHash string    `json:"transaction_hash"`
+	UpdatedAt       time.Time `json:"updated_at"`
+}
+
+func ReadDeposits(ctx context.Context, chain int64, offset int64) ([]*Deposit, error) {
+	data, err := Request(ctx, "GET", fmt.Sprintf("/deposits?chain=%d&offset=%d", chain, offset), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var deposits []*Deposit
+	err = json.Unmarshal(data, &deposits)
+	if err != nil {
+		return nil, err
+	}
+	return deposits, nil
+}
---
+++ client/deposit_test.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDepositRPC(t *testing.T) {
+	assert := assert.New(t)
+	ctx := context.Background()
+
+	deposits, err := ReadDeposits(ctx, 1, 0)
+	assert.Nil(err)
+	assert.True(len(deposits) > 0)
+}
