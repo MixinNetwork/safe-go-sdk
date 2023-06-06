36d5866e0d3197243ae80035894fb533e661b35d
LiYuqing <im.yuqlee@gmail.com>
2023-06-06 16:47:24 +0800

    More tests for tx

--- operation/operation_test.go
+++ operation/operation_test.go
@@ -26,7 +26,13 @@ func TestOperation(t *testing.T) {
 	err = VerifySafeMessage(hex.EncodeToString(pub.SerializeCompressed()), hash, sigBuf)
 	assert.Nil(err)
 
+	// APPROVE:ACCOUNT-ID:ADDRESS
 	sigBuf, _ = base64.RawURLEncoding.DecodeString("MEUCIQDpVx9JXZDNTp17E1LVBbD7BSkrNxw4V5Y00z0jsY2oDQIgKGk7RVIjO2NEvoLn5fotX7j4Kc4YdYI3ueq7GlmAWyc")
 	hash = HashMessageForSignature("APPROVE:8aef8130-aa9c-418a-871d-e920fed2f0e4:bc1qd4qjpy8n3gksd56aqc9pz36tpy26ev2sq93xwkz3qjk64dra8ruq5p5hyv", 1)
 	err = VerifySafeMessage("0339af9aed5542535f1c609d45847ddc56d0d469cb59a3bcddf6555e028e42457e", hash, sigBuf)
+
+	// APPROVE|REVOKE:TX-ID:TX-HASH
+	sigBuf, _ = base64.RawURLEncoding.DecodeString("MEQCIE5JQAc8yY6RtN1WXl4FpSSKT66ck1Vs397g-BoGPHVAAiByaZ5hviSmsN1wiWPUhyetsC4wqpPiYFOlplWfylzRVA")
+	hash = HashMessageForSignature("APPROVE:3ec57759-4bc9-4084-99f2-c712f1da31db:eaac19b6879b99cfab35b6f7f52d421eebaf2256f7ec4b3cdde2c1240bbe63ff", 1)
+	err = VerifySafeMessage("02b06814acd1b5993450c3732e4d8d5be8a19bf102461a5ccdfcc4926bed504b0a", hash, sigBuf)
 }
